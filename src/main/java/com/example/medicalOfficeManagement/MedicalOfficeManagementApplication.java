a3816176eed8da05695b98563944f7239dd550c4
yasminehamadene <120636233+yasminehamadene@users.noreply.github.com>
2024-12-19 13:09:03 +0100

    premier commit de projet

---
+++ src/main/java/com/example/medicalOfficeManagement/MedicalOfficeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalOfficeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalOfficeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalOfficeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicalOfficeManagement/MedicalOfficeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalOfficeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalOfficeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
